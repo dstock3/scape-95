2ad2772988e7d68e939011e8f689c1784545eb8e
David Stockdale <dstock3@gmail.com>
2021-12-16 11:14:49 -0600

    Added a findPage method to the Internet component, which checks a pageList bit of state that is initialized when the component is rendered. If there are no matches, it renders a component called NotFound.

--- src/components/Windows/internet/Internet.js
+++ src/components/Windows/internet/Internet.js
@@ -1,17 +1,37 @@
 import React, { useState } from 'react'
 import Homepage from './Homepage'
+import NotFound from './NotFound'
 
 function Internet() {
-    const [page, setPage] = useState({current: <Homepage />, pageID: "homepage"})
+    const [pageList, setPageList] = useState([
+        {component: <Homepage />, id: "homepage", url: "http://www.scape.net"},
+        {component: <NotFound />, id: "not-found", url: ""},
+    ])
+
+    const [page, setPage] = useState({current: <Homepage />, pageID: "homepage", url: "http://www.scape.net"})
 
     const setHome = () => {
-        setPage({...page, current: <Homepage />, pageID: "homepage"})
+        setPage({...page, current: <Homepage />, pageID: "homepage", url: "http://www.scape.net"})
     }
     
     const refresh = () => {
         setPage({...page, current: page.current, pageID: page.pageID})
     }
     
+    const findPage = e => {
+        e.preventDefault();
+        let match = false
+        for (let i = 0; i < pageList.length; i++) {
+            if (e.target.value === pageList[i].url) {
+                match = true
+                setPage({...page, current: pageList.component, pageID: pageList.id, url: pageList.url})
+            }
+        }
+        if (!match) {
+            setPage({...page, current: <NotFound />, pageID: "not-found", url: ""})
+        }
+    }
+
     return (
         <div className="internet">
             <div className="net-header">
@@ -24,8 +44,7 @@ function Internet() {
                 </div>
                 <div className="net-input-container">
                     <div className="net-location">Location:</div>
-                    <input className="net-input"></input>
-
+                    <input className="net-input" type="text" value={page.url} onChange={findPage} />
                 </div>
             </div>
             <div id={page.pageID}>
