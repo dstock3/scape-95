db335211cf21e0c05d093e5ba6fae9ec0b7ccddc
David Stockdale <dstock3@gmail.com>
2021-12-19 15:44:07 -0600

    Added style attributes to Loading component.

--- src/components/Windows/internet/Loading.js
+++ src/components/Windows/internet/Loading.js
@@ -1,8 +1,13 @@
 import React from 'react'
 
 function Loading() {
+    const loadStyle = {
+        backgroundColor: "white",
+        height: "100%",
+        width: "100%"
+    }
     return (
-        <div className="loading-page">
+        <div className="loading-page" style={loadStyle}>
             Is Loading   
         </div>
     )
