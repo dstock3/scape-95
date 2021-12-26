1bcad71197a36790da0c750b289feb0749a0d6d0
David Stockdale <dstock3@gmail.com>
2021-12-26 12:21:02 -0600

    Employed map method to generate list items in Bookmarks component.

--- src/components/Windows/internet/Bookmarks.js
+++ src/components/Windows/internet/Bookmarks.js
@@ -7,16 +7,30 @@ function Bookmarks(props) {
         setIsOpen(true)
     }
 
-    return (
-        <div className="bookmarks-button" onClick={reveal}>
-            Bookmarks
-            <div className="bookmarks-list">
-                {
-
-                }
+    if (isOpen) {
+        return (
+            <div className="bookmarks">
+                <div className="bookmarks-button" onClick={reveal}>
+                    Bookmarks
+                </div>
+                <ul className="bookmarks-list">
+                    {props.bookmarks.map(item => (
+                        <li className="net-list-item" key={item.key}>
+                            <a href={item.link}>{item.linkTitle}</a>
+                        </li>
+                    ))}
+                </ul>
             </div>
-        </div>
-    )
+        )
+    } else {
+        return (
+            <div className="bookmarks">
+                <div className="bookmarks-button" onClick={reveal}>
+                    Bookmarks
+                </div>
+            </div>
+        )
+    }
 }
 
 export default Bookmarks
