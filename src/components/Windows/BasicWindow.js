265536d1042ca8dda4c8dd34b9abfadc0c1bf0a9
David Stockdale <dstock3@gmail.com>
2021-12-30 13:41:42 -0600

    Set draggable to false on shortcut image element to ensure that the user does drag the image itself, but rather the entire parent element.

--- src/components/Windows/BasicWindow.js
+++ src/components/Windows/BasicWindow.js
@@ -89,7 +89,7 @@ function BasicWindow(props) {
         return (
             <>
                 <div className={`basic-window ${isHidden}`} id={props.winId} draggable={win.isDraggable} onDragStart={newDrag} style={win.style}>
-                    <div className="window-top" onMouseEnter={()=> setWin({...win, isDraggable: true})} onMouseLeave={()=> setWin({...win, isDraggable: false})}onDoubleClick={maxToggle}>
+                    <div className="window-top" onMouseEnter={()=> setWin({...win, isDraggable: true})} onMouseLeave={()=> setWin({...win, isDraggable: false})} onDoubleClick={maxToggle}>
                         <div className="window-title">{props.winTitle}</div>
                         <WindowsButtons min={props.min} max={maxToggle} close={closeSet} />
                     </div>
--- src/components/desktop_shortcuts/DesktopIcon.js
+++ src/components/desktop_shortcuts/DesktopIcon.js
@@ -1,10 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { newDrag } from '../../DragFunctions'
 
 const DesktopIcon = (props) => {
+
     return (
         <div className="shortcut" onDoubleClick={props.open} id={props.shortcutId} draggable="true" onDragStart={newDrag}>
-            <img className="desktop-icon" id={props.shortcutIconId} src={props.imgSrc} alt={props.shortcut}></img>
+            <img className="desktop-icon" draggable={false} id={props.shortcutIconId} src={props.imgSrc} alt={props.shortcut}></img>
             <div className="folder-name">{props.shortcut}</div>
         </div>
     )
