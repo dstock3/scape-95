a720c6926f9b0fe029a1224020ac56b79528d1c3
David Stockdale <dstock3@gmail.com>
2022-01-11 17:01:27 -0600

    Added basic functionality to Terminal component.

--- src/components/Windows/Terminal.js
+++ src/components/Windows/Terminal.js
@@ -1,21 +1,43 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import '../../style/terminal.css'
 
 function Terminal(props) {
     const [dir, setDir] = useState(`C:\\SCAPE-95>`)
     const [dirInput, setDirInput] = useState("")
     const [dirArray, setDirArray] = useState([])
+    const [caret, setCaret] = useState("caret-active")
 
     const handleSubmit = e => {
         e.preventDefault();
-
+        let check = false
         for (let prop in props.openApps) {
             if (dirInput === prop) {
+                check = true
                 props.openApps[prop]()
+            } else if (dirInput === "cd..") {
+                check = true
+                setCaret("caret-active")
+                setDir(`C:\\>`)
             }
         }
-        setDirArray([...dirArray, dirInput])
-        console.log(dirArray)
+        if (!check) {
+            setCaret("caret-active")
+            setDirArray([...dirArray, {path: dir, content: dirInput}, {path: "", content: "command not recognized"}])
+            setDirInput("")
+        } else {
+            setCaret("caret-active")
+            setDirArray([...dirArray, {path: dir, content: dirInput}])
+            setDirInput("")
+        }
+    }
+
+    const changeHandler = e => {
+        setCaret("caret-inactive")
+        setDirInput(e.target.value)
+
+        if (e.target.value === "") {
+            setCaret("caret-active")
+        } 
     }
 
     return (
@@ -25,12 +47,18 @@ function Terminal(props) {
                 
                 <div className="copyright">(C)ScapeNet Corp 1981-1999.</div>
             </div>
+            {dirArray.map(item => (
+                <div className="line-input">
+                    <span className="dir">{item.path}</span> 
+                    <span>{item.content}</span>
+                </div>
+            ))}
             <div className="line-item">
                 <form className="line-input" onSubmit={handleSubmit}>
                     <label className="dir">{dir}</label>   
-                    <span className="caret"></span>                 
+                    <span className={caret}></span>                 
                     
-                    <input className="terminal-input" value={dirInput} onChange={e => setDirInput(e.target.value)} > 
+                    <input className="terminal-input" value={dirInput} onChange={changeHandler} > 
                     </input>
                 </form>
             </div>
