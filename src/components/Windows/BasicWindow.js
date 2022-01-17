0885102e093272c493d687cf323052ca7791120f
David Stockdale <dstock3@gmail.com>
2022-01-17 14:04:26 -0600

    Exploring possibility of using reducer to manage state in BasicWindow component.

--- src/components/Windows/BasicWindow.js
+++ src/components/Windows/BasicWindow.js
@@ -1,12 +1,135 @@
-import React, { useState, useEffect, useRef } from 'react'
+import React, { useState, useEffect, useRef, useReducer } from 'react'
 import { newDrag } from '../../DragFunctions'
 import '../../style/window.css'
 import WindowsButtons from './WindowsButtons'
 
-function BasicWindow(props) {
+
+const ACTIONS = {
+    MAX: 'max',
+    DEF: 'def',
+    DRAG: 'drag',
+    NODRAG:"nodrag",
+    MOVE: "move",
+    NOMOVE: "nomove"
+}
+
+function reducer(win, action) {
+    const main = document.querySelector(".main")
+    let maxHeight = main.offsetHeight - 40
+    const maxWidth = main.offsetWidth
+    let mql = window.matchMedia('(max-width: 800px)');
+    
     let defaultWidth
     let defaultHeight
+    
+    if (win.size) {
+        defaultWidth = win.size.width
+        defaultHeight = win.size.height
+    } else {
+        defaultWidth = "650px"
+        defaultHeight ="650px"
+    }
+    
+    let defaultStyle
+    if (mql.matches) {
+        console.log("match")
+        defaultStyle = {
+            position: "absolute",
+            left: "0",
+            top: "0",
+            minHeight: maxHeight + "px",
+            minWidth: maxWidth + "px",
+            zIndex: 1,
+        }
+    } else {
+        defaultStyle = {
+            position: "relative",
+            left: "0",
+            top: "350px",
+            bottom: "0",
+            minHeight: defaultHeight,
+            minWidth: defaultWidth,
+            zIndex: 1,
+        }
+    }
 
+    let selectStyle = {
+        position: "relative",
+        right: "550px",
+        bottom: "500px",
+        minHeight: defaultHeight,
+        minWidth: defaultHeight,
+    }
+
+    let maxStyle = {
+        position: "fixed",
+        minHeight: maxHeight + "px",
+        minWidth: maxWidth + "px",
+        left: "0",
+        top: "0",
+        zIndex: 2
+    }
+
+    let defaultBody = {
+        height: parseInt(defaultHeight.replace("px", "") - 25) + "px"
+    }
+
+    let maxBody = {
+        height: main.offsetHeight - 15
+    }
+
+    switch(action.type) {
+        case ACTIONS.MAX :
+            return {
+                isMoved: false,
+                isMax: true, 
+                isDraggable: false,
+                isSelected: true,
+                style: maxStyle,
+                bodyStyle: maxBody
+            }
+        case ACTIONS.DEF :
+            return {
+                isMax: false, 
+                isDraggable: false,
+                style: defaultStyle,
+                bodyStyle: defaultBody
+            }
+        case ACTIONS.SEL :
+            return {
+                isMax: false, 
+                isDraggable: false,
+                style: selectStyle,
+                bodyStyle: defaultBody
+            }
+        case ACTIONS.DRAG :
+            return {
+                isDraggable: true,
+            }
+        case ACTIONS.NODRAG :
+            return {
+                isDraggable: false,
+            }
+        case ACTIONS.MOVE :
+            return {
+                isMoved: true,
+            }
+        case ACTIONS.NOMOVE :
+            return {
+                isMoved: false,
+            }
+
+        default:
+            return win
+
+    }
+}
+
+function BasicWindow(props) {
+    
+    let defaultWidth
+    let defaultHeight
+    
     if (props.size) {
         defaultWidth = props.size.width
         defaultHeight = props.size.height
@@ -15,20 +138,14 @@ function BasicWindow(props) {
         defaultHeight ="650px"
     }
 
-    const [isMoved, setMoved] = useState(false)
-    const parentRef = useRef(false)
-
-    useEffect(()=> {
-        let win = document.getElementById(props.winId)
-        parentRef.current = win.parentElement
-
-    }, [])
-
-    const [win, setWin] = useState({
+    const [win, dispatch] = useReducer(reducer, {
+        mql: window.matchMedia('(max-width: 800px)'),
+        size: props.size,
         isMax: false, 
         isDraggable: false,
         isSelected: true,
         style: {
+            isMoved: false,
             position: "relative",
             right: "550px",
             bottom: "500px",
@@ -39,6 +156,16 @@ function BasicWindow(props) {
             height: parseInt(defaultHeight.replace("px", "") - 25)
         }
     })
+
+    const parentRef = useRef(false)
+
+    useEffect(()=> {
+        let win = document.getElementById(props.winId)
+        parentRef.current = win.parentElement
+
+    }, [])
+    
+
 
     const [isHidden, setHidden] = useState("hidden")
 
@@ -57,85 +184,23 @@ function BasicWindow(props) {
         props.close()
     }
     
-    const styleController = () => {
-        const main = document.querySelector(".main")
-        let maxHeight = main.offsetHeight - 40
-        const maxWidth = main.offsetWidth
-
-        const defaultStyle = {
-            position: "relative",
-            left: "0",
-            top: "350px",
-            bottom: "0",
-            minHeight: defaultHeight,
-            minWidth: defaultWidth,
-            zIndex: 1,
-        }
-
-        const selectStyle = {
-            position: "relative",
-            right: "550px",
-            bottom: "500px",
-            minHeight: defaultHeight,
-            minWidth: defaultHeight,
-        }
-
-        const maxStyle = {
-            position: "fixed",
-            minHeight: maxHeight + "px",
-            minWidth: maxWidth + "px",
-            left: "0",
-            top: "0",
-            zIndex: 2
-        }
-
-        const defaultBody = {
-            height: parseInt(defaultHeight.replace("px", "") - 25) + "px"
-        }
-
-        const maxBody = {
-            height: main.offsetHeight - 15
-        }
-
-        return { defaultStyle, maxStyle, defaultBody, selectStyle, maxBody }
-    }
-
     const maxToggle = () => {
-        const newStyle = styleController()
         if (win.isMax) {
-            if (isMoved) {
-                setWin({ ...win, 
-                    isMin: false, 
-                    isMax: false, 
-                    isDraggable: false,
-                    style: newStyle.defaultStyle,
-                    bodyStyle: newStyle.defaultBody
-                })
+            if (win.isMoved) {
+                dispatch({ type: ACTIONS.DEF })
 
             } else {
-                setWin({ ...win, 
-                    isMin: false, 
-                    isMax: false, 
-                    isDraggable: false,
-                    style: newStyle.selectStyle,
-                    bodyStyle: newStyle.defaultBody
-                })
+                dispatch({ type: ACTIONS.SEL })
             }
         } else {
-            setWin({ ...win, 
-                isMin: false, 
-                isMax: true, 
-                isDraggable: false,
-                style: newStyle.maxStyle,
-                bodyStyle: newStyle.maxBody
-            })
+            dispatch({ type: ACTIONS.MAX })
         }
     }
 
     useEffect(()=> {
         let window = document.getElementById(props.winId)
         if (parentRef.current !== window.parentElement) {
-            setMoved(true)
+            dispatch({ type: ACTIONS.MOVE })
         }
 
         if (win.isMax) {
@@ -149,7 +214,7 @@ function BasicWindow(props) {
 
     const setDraggableTrue = () => {
         if (!win.isMax) {
-            setWin({...win, isDraggable: true})
+            dispatch({ type: ACTIONS.DRAG })
         }
     }
 
@@ -176,7 +241,7 @@ function BasicWindow(props) {
         return (
             <>
                 <div className={`basic-window ${isHidden}`} id={props.winId} draggable={win.isDraggable} onDragStart={newDrag} style={win.style}>
-                    <div className="window-top" onMouseEnter={setDraggableTrue} onMouseLeave={()=> setWin({...win, isDraggable: false})} onDoubleClick={maxToggle}>
+                    <div className="window-top" onMouseEnter={setDraggableTrue} onMouseLeave={()=> dispatch({ type: ACTIONS.NODRAG })} onDoubleClick={maxToggle}>
                         <div className="window-title">{props.winTitle}</div>
                         <WindowsButtons min={props.min} max={maxToggle} close={closeSet} />
                     </div>
