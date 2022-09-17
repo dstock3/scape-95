ab9043058f025514cbf746fc8e6baf4cc87d013f
David Stockdale <dstock3@gmail.com>
2022-09-17 17:29:41 -0500

    Removed style assignment object within Loading component and added specifications to corresponding stylesheet.

--- src/components/Windows/internet/pages/Loading.js
+++ src/components/Windows/internet/pages/Loading.js
@@ -2,13 +2,9 @@ import React from 'react'
 import "../../../../style/loading.css"
 
 function Loading() {
-    const loadStyle = {
-        backgroundColor: "black",
-        height: "100%",
-        width: "100%"
-    }
+
     return (
-        <div className="loading-page" style={loadStyle}>
+        <div className="loading-page">
             <div className="loading-container">
                 <div className="loading"></div>
             </div>  
