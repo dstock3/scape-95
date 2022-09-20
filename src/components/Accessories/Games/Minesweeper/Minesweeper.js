04e33c7661a3e3b617544ae33a48271262604f3c
David Stockdale <dstock3@gmail.com>
2022-09-20 17:57:35 -0500

    Started working on Minesweeper component and corresponding stylesheet.

--- src/components/Accessories/Games/Minesweeper/Minesweeper.js
+++ src/components/Accessories/Games/Minesweeper/Minesweeper.js
@@ -1,11 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
+import '../../../../style/games/minesweeper.css'
 
-function Minesweeper() {
+const Minesweeper = () => {
+    const [mineNum, setMineNum] = useState(10)
+
     return (
         <div className="minesweeper">
-            
+            <div className="top-lvl">
+                <div className="mine-num">
+                    {mineNum}
+                </div>
+                <div className="smiley">
+                    {/* need to find smiley icon*/}
+                </div>
+                <div className="mine-timer">
+                    
+                </div>
+            </div>
+            <div className="mine-grid">
+
+            </div>
         </div>
     )
 }
 
 export default Minesweeper
+
