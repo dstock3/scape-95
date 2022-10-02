dea3d8f1b719a9bef692e95fa4ba32d5113d3fc8
David Stockdale <dstock3@gmail.com>
2022-10-02 17:30:32 -0500

    Added icons to SettingsMenu component.

--- src/components/startbar/SettingsMenu.js
+++ src/components/startbar/SettingsMenu.js
@@ -1,6 +1,9 @@
 import React, { useState, useContext } from 'react'
 import '../../style/start.css'
 import { StartContext } from '../Main'
+import controlIcon from '../../assets/icons/control.png'
+import printerIcon from '../../assets/icons/print.png'
+import taskbarIcon from '../../assets/icons/taskbar.png'
 
 const SettingsMenu = (props) => {
     const openApps = useContext(StartContext)
@@ -8,7 +11,7 @@ const SettingsMenu = (props) => {
     const [progStyle, setProgStyle] = useState({
         position: "absolute",
         left: "16.9%",
-        bottom: "775%",
+        bottom: "465%",
         borderLeft: "1px black solid",
         zIndex: "4",
         height: "fit-content",
@@ -23,7 +26,26 @@ const SettingsMenu = (props) => {
 
     if (props.isClicked) {
         return (
-            <div>SettingsMenu</div>
+            <div className="start-menu" style={progStyle}>
+                <div className="start-option-container">
+                    <div className="start-option">
+                        <img src={controlIcon} alt="control panel icon" className="start-item-icon"></img>
+                        <div className="start-item"><span style={{textDecoration: "underline"}}>C</span>ontrol Panel</div>
+                    </div>
+                </div>
+                <div className="start-option-container">
+                    <div className="start-option">
+                        <img src={printerIcon} alt="printers icon" className="start-item-icon"></img>
+                        <div className="start-item"><span style={{textDecoration: "underline"}}>P</span>rinter</div>
+                    </div>
+                </div>
+                <div className="start-option-container">
+                    <div className="start-option">
+                        <img src={taskbarIcon} alt="taskbar icon" className="start-item-icon"></img>
+                        <div className="start-item"><span style={{textDecoration: "underline"}}>T</span>askbar</div>
+                    </div>
+                </div>
+            </div>
         )
     } else {
         return null
