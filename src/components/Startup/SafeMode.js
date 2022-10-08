4a7cafff9950afce7a6d099dd52f349fe497d50d
David Stockdale <dstock3@gmail.com>
2022-10-08 17:02:07 -0500

    Made stylistic adjustments to safe mode elements.

--- src/components/Startup/SafeMode.js
+++ src/components/Startup/SafeMode.js
@@ -4,15 +4,17 @@ import '../../style/safe-mode.css'
 const SafeMode = () => {
   return (
     <div className="safe-mode">
+      <div className="safe-mode-menu">
         <h1 className="safe-mode-title">Scape 95 Startup Menu</h1>
-        <ul className="safe-mode-options">
-            <li className="safe-mode-option"></li>
-            <li className="safe-mode-option"></li>
-            <li className="safe-mode-option"></li>
-        </ul>
-        <div className="safe-mode-msg">
-            Warning: Scape did not finish loading on the previous attempt. Choose Safe mode, to start Windows with a minimal set of drivers.
-        </div>
+          <ul className="safe-mode-options">
+              <li className="safe-mode-option">1.</li>
+              <li className="safe-mode-option">2.</li>
+              <li className="safe-mode-option">3.</li>
+          </ul>
+          <div className="safe-mode-msg">
+              Warning: Scape did not finish loading on the previous attempt. Choose Safe mode, to start Windows with a minimal set of drivers.
+          </div>
+      </div>
     </div>
   )
 }
