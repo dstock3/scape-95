7a29e2ea23deb30e0dd8c8d13bf9d7a0a69eac09
David Stockdale <dstock3@gmail.com>
2022-10-10 16:53:44 -0500

    Added useEffect hook in Loading component with setTimeout call to simulate loading time.

--- src/App.js
+++ src/App.js
@@ -5,14 +5,12 @@ import Loading from './components/Startup/Loading';
 import SafeMode from './components/Startup/SafeMode';
 
 function App() {
-  const [startup, setStartup] = useState(1)
+  const [startup, setStartup] = useState("start")
   return (
     <div className="App">
-      <Loading />
-      {/*
       <SafeMode startup={startup} setStartup={setStartup} />
-      {startup === 1 ? <Main setStartup={setStartup} /> : null}
-      */}
+      {startup === 1 ? <Loading setStartup={setStartup} /> : null}
+      {startup === "start" ? <Main setStartup={setStartup} /> : null}
     </div>
   );
 }
--- src/components/Startup/Loading.js
+++ src/components/Startup/Loading.js
@@ -1,8 +1,15 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import loadingScreen from '../../assets/system/startup.gif'
 import '../../style/startup.css'
 
-const Loading = () => {
+const Loading = ({setStartup}) => {
+  
+  useEffect(()=> {
+      setTimeout(() => {
+        setStartup("start")
+      }, "5000")
+  }, [])
+
   return (
     <div className="loading-screen">
       <img className="loading-img" src={loadingScreen} alt="loading screen"></img>
