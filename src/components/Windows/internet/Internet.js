869620a5a5317ad6000ad19fcffe4a88c624f763
David Stockdale <dstock3@gmail.com>
2023-11-22 17:32:31 -0600

    added new grid page template to browser.

--- src/components/Windows/internet/Internet.js
+++ src/components/Windows/internet/Internet.js
@@ -10,6 +10,7 @@ import WebGames from './pages/WebGames'
 import TicTacToe from './pages/games/TicTacToe'
 import Bookmarks from './containers/Bookmarks'
 import InputContainer from './containers/InputContainer'
+import GridLayout from './pages/GridLayout'
 
 function Internet() {
     useEffect(()=> {
@@ -29,7 +30,8 @@ function Internet() {
         {component: <NewPage colPosition="col-right"/>, title: "New Page", id: "newpage", url: "http://www.newpage.com"},
         {component: <NewPage2 />, title: "New Page 2", id: "newpage2", url: "http://www.newpage2.com"},
         {component: <NotFound />, title: "404 Not Found", id: "not-found", url: ""},
-        {component: <TicTacToe />, title: "Tic-Tac-Toe", id: "tic-tac-toe", url:  "http://www.webgames.com/tictactoe"}
+        {component: <TicTacToe />, title: "Tic-Tac-Toe", id: "tic-tac-toe", url:  "http://www.webgames.com/tictactoe"},
+        {component: <GridLayout />, title: "Grid Layout", id: "grid-layout", url:  "http://www.gridlayout.com"}
     ])
 
     const bookmarks = [
@@ -37,7 +39,8 @@ function Internet() {
         {component: <WebGames linkOne={setTic}/>, title: "Web Games", id: "games", url: "http://www.webgames.com"},
         {component: <NewPage colPosition="col-right"/>, title: "New Page", id: "newpage", url: "http://www.newpage.com"},
         {component: <NewPage2 />, title: "New Page 2", id: "newpage2", url: "http://www.newpage2.com"},
-        {component: <TicTacToe />, title: "Tic-Tac-Toe", id: "tic-tac-toe", url:  "http://www.webgames.com/tictactoe"}
+        {component: <TicTacToe />, title: "Tic-Tac-Toe", id: "tic-tac-toe", url:  "http://www.webgames.com/tictactoe"},
+        {component: <GridLayout />, title: "Grid Layout", id: "grid-layout", url:  "http://www.gridlayout.com"}
     ]
 
     const homePage = {current: <Homepage />, title: "ScapeNet", pageID: "homepage", url: "http://www.scape.net"}
---
+++ src/components/Windows/internet/pages/GridLayout.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import '../../../../style/grid.css'
+
+const GridLayout = () => {
+  return (
+    <div className="grid-container">
+        <header className="grid-header">
+            <h1 className="grid-title">Header Content</h1>
+        </header>
+        <hr className="grid-divider"/>
+        <div className="grid-item">
+            <h2>Grid Item 1</h2>
+            <p>Lorem ipsum dolor sit amet...</p>
+            <a href="#" className="grid-link">More Info</a>
+            <button className="grid-button">Click Me</button>
+        </div>
+        <hr className="grid-divider"/>
+        <div className="grid-item">
+            <h2>Grid Item 2</h2>
+            <p>Lorem ipsum dolor sit amet...</p>
+            <a href="#" className="grid-link">More Info</a>
+            <button className="grid-button">Click Me</button>
+        </div>
+        <hr className="grid-divider"/>
+        <div className="grid-item">
+            <h2>Grid Item 3</h2>
+            <p>Lorem ipsum dolor sit amet...</p>
+            <a href="#" className="grid-link">More Info</a>
+            <button className="grid-button">Click Me</button>
+        </div>
+        <hr className="grid-divider"/>
+        <footer className="grid-footer">
+            <p>Footer content goes here...</p>
+        </footer>
+    </div>
+  );
+}
+
+export default GridLayout;
