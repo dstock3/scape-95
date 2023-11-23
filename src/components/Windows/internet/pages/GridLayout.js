12a797dd63a9db6a7b47b0714a799c0fe25b3f5b
David Stockdale <dstock3@gmail.com>
2023-11-22 18:22:24 -0600

    made stylistic adjustments to grid page.

--- src/components/Windows/internet/pages/GridLayout.js
+++ src/components/Windows/internet/pages/GridLayout.js
@@ -11,22 +11,23 @@ const GridLayout = () => {
             <hr className="grid-divider"/>
             <div className="grid-item">
                 <h2>Grid Item 1</h2>
-                <p>Lorem ipsum dolor sit amet...</p>
-                <a href="#" className="grid-link">More Info</a>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                 <button className="grid-button">Click Me</button>
             </div>
             <hr className="grid-divider"/>
             <div className="grid-item">
                 <h2>Grid Item 2</h2>
-                <p>Lorem ipsum dolor sit amet...</p>
-                <a href="#" className="grid-link">More Info</a>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                 <button className="grid-button">Click Me</button>
             </div>
             <hr className="grid-divider"/>
             <div className="grid-item">
                 <h2>Grid Item 3</h2>
-                <p>Lorem ipsum dolor sit amet...</p>
-                <a href="#" className="grid-link">More Info</a>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+
                 <button className="grid-button">Click Me</button>
             </div>
             <hr className="grid-divider"/>
