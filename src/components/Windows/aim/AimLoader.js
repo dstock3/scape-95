32ec811e44e204d460c47e44ec4f45a1fff2c6b2
dstock3 <dstock3@gmail.com>
2023-11-26 16:38:09 -0600

    Enhanced AimLoader with realistic step-by-step loading process

--- src/components/Windows/aim/AimLoader.js
+++ src/components/Windows/aim/AimLoader.js
@@ -1,19 +1,36 @@
-
 import React, { useState, useEffect } from 'react';
 import '../../../style/aim.css'; 
 import aimSignInImg from '../../../assets/aim/aim-sign-in.png'; 
 
 const AimLoader = ({ closeAimLoader, openAimClient }) => {
-  const [isLoading, setIsLoading] = useState(true);
+  const [loadingStep, setLoadingStep] = useState(1);
 
   useEffect(() => {
-    const timer = setTimeout(() => {
-      setIsLoading(false);
-      openAimClient();
-      closeAimLoader();
-    }, 5000);
-    return () => clearTimeout(timer);
+    const totalLoadingTime = 5000; 
+    const steps = 3; 
+    const stepDuration = totalLoadingTime / steps;
+
+    const timers = [
+      setTimeout(() => setLoadingStep(2), stepDuration), 
+      setTimeout(() => setLoadingStep(3), stepDuration * 2), 
+      setTimeout(() => {
+        setLoadingStep(0); 
+        openAimClient();
+        closeAimLoader();
+      }, totalLoadingTime)
+    ];
+
+    return () => timers.forEach(timer => clearTimeout(timer));
   }, []);
+
+  const renderLoadingMessage = () => {
+    switch (loadingStep) {
+      case 1: return "Connecting...";
+      case 2: return "Verifying name and password...";
+      case 3: return "Starting Services...";
+      default: return "AIM loaded successfully!";
+    }
+  };
 
   return (
     <div className="aim-loader-container">
@@ -21,11 +38,7 @@ const AimLoader = ({ closeAimLoader, openAimClient }) => {
             <img src={aimSignInImg} alt="AIM Logo" className="aim-logo" />
         </div>
         <hr className="aim-hr" />
-        {isLoading ? (
-            <div>Loading AIM...</div>
-            ) : (
-            <div>AIM loaded successfully!</div>
-        )}
+        <div className="aim-loading-message">{renderLoadingMessage()}</div>
         <div className="aim-buttons-container">
             <button className="aim-cancel-btn" onClick={closeAimLoader}>Cancel</button>
         </div>
