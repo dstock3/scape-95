a1d0e4d021ca74455c4308decef1bfea6bf58dd9
dstock3 <dstock3@gmail.com>
2023-12-05 15:12:54 -0600

    started fleshing out AimWindow component.

--- src/components/Windows/aim/AimWindow.js
+++ src/components/Windows/aim/AimWindow.js
@@ -1,20 +1,42 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+import Quill from 'quill';
+import 'quill/dist/quill.snow.css';
 import '../../../style/aim.css';
 
 const AimWindow = () => {
     const [messages, setMessages] = useState([]);
-    const [newMessage, setNewMessage] = useState('');
+    const quillRef = useRef(null); 
+
+    useEffect(() => {
+        if (quillRef.current) {
+            const quill = new Quill(quillRef.current, {
+                theme: 'snow',
+                modules: {
+                    toolbar: [['bold', 'italic', 'underline', 'link', 'image']] 
+                },
+            });
+
+            quill.on('text-change', () => {
+                setMessages([...messages, { text: quill.root.innerHTML, sent: true }]);
+            });
+        }
+    }, [quillRef, messages]);
 
     const handleSendMessage = () => {
-        if (newMessage.trim() !== '') {
-            setMessages([...messages, newMessage]);
-            setNewMessage('');
+        if (quillRef.current) {
+            const quill = new Quill(quillRef.current);
+            const messageContent = quill.root.innerHTML;
+            if (messageContent.trim() !== '<p><br></p>') { 
+                const timestamp = new Date().toLocaleTimeString();
+                setMessages([...messages, { text: messageContent, sent: true, time: timestamp }]);
+                quill.root.innerHTML = ''; 
+            }
         }
     };
 
     return (
         <div className="aim-window">
-            <ul className="aim-window-file-options">
+            <ul className="aim-menu-bar">
                 <li className="aim-window-file-option">
                     <span style={{textDecoration: "underline"}}>F</span>ile
                 </li>
@@ -28,25 +50,26 @@ const AimWindow = () => {
                     <span style={{textDecoration: "underline"}}>P</span>eople
                 </li>
             </ul>
-            <div className="aim-chat-area">
+
+            <hr className="aim-menu-bar-divider" />
+
+            <div className="aim-message-area">
                 {messages.map((message, index) => (
-                    <div key={index} className="aim-message">
-                        {message}
+                    <div key={index} className={`message ${message.sent ? 'sent' : 'received'}`}>
+                        <span>[{message.time}] </span>
+                        <span className="username">{message.sent ? 'You' : 'Friend'}: </span>
+                        <span>{message.text}</span>
                     </div>
                 ))}
             </div>
-
+            <div className="aim-toolbar"></div>
             <div className="aim-message-input-area">
-                <input
-                    type="text"
-                    value={newMessage}
-                    onChange={(e) => setNewMessage(e.target.value)}
-                    placeholder="Type a message..."
-                />
+                <div ref={quillRef} style={{ height: 100 }}></div> 
                 <button onClick={handleSendMessage}>Send</button>
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default AimWindow;
+
